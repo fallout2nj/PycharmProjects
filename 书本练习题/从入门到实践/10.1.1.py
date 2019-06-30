0dcd392ec5c14bcef59f881be7035f680a55721a
fallo <fallout.nj@gmail.com>
2019-06-30 15:54:26 +0800

    加入写代码段

--- 书本练习题/从入门到实践/10.1.1.py
+++ 书本练习题/从入门到实践/10.1.1.py
@@ -1,3 +1,5 @@
+'''↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓读文件块↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓'''
+
 # with open('pi_digits.txt') as file_object:
 #     contents = file_object.read()
 #     print(contents)
@@ -50,16 +52,36 @@
 # print(len(pi_string))
 
 
-'''圆周率里包含你的生日吗？'''
+# 圆周率里包含你的生日吗？
+# filename = 'pi_million_digits.txt'
+# with open(filename) as file_object:
+#     lines = file_object.readlines()
+# pi_string = ''
+# for each_line in lines:
+#     pi_string += each_line.strip()
+# birthday = input("Enter your birthday, in the form mmddyy:")
+# if birthday in pi_string:
+#     print("Your birthday appears in the first million digits of Pi!")
+# else:
+#     print("Your birthday does not appear in the first million digits of Pi.")
 
-filename = 'pi_million_digits.txt'
-with open(filename) as file_object:
-    lines = file_object.readlines()
-pi_string = ''
-for each_line in lines:
-    pi_string += each_line.strip()
-birthday = input("Enter your birthday, in the form mmddyy:")
-if birthday in pi_string:
-    print("Your birthday appears in the first million digits of Pi!")
-else:
-    print("Your birthday does not appear in the first million digits of Pi.")
+'''↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑读文件块↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑'''
+
+
+
+
+
+
+'''↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓写文件块↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓'''
+
+# filename = 'programming.txt'
+# with open(filename, 'w') as file_object:
+#     file_object.write("I love programming.")  # 这里注意双引号是个小技巧，便于英文中的单引号的出现
+
+
+filename = 'programming.txt'
+with open(filename, 'a') as file_object:
+    file_object.write("I love programming too.\n")
+    file_object.write("I love creating new games.\n")  # 结尾处务必带上换行符，上面代码段因为没有所以和第二段连在一起了
+
+'''↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑写文件块↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑'''
