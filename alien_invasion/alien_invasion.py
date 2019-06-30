e14e889de6d3503f2e3f074b6202ee469939b2c0
fallo <fallout.nj@gmail.com>
2019-07-01 02:07:34 +0800

    游戏的初期窗口属性
    2019年6月30日夜间

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,11 +1,9 @@
-import sys
-
 import pygame
 
+from pygame.sprite import Group
 from settings import Settings
-
 from ship import Ship
-
+import game_functions as gf
 
 def run_game():
 
@@ -18,26 +16,25 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # 创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     # screen = pygame.display.set_mode((1200, 800))
     # pygame.display.set_caption("Alien Invasion")
 
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
+
     # 设置背景色
     bg_color = (230, 230, 230)
+
+    # 开始游戏主循环
     while True:
 
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
 
-        # 每次循环都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        # screen.fill(bg_color)
-
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
