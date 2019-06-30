a2895d22a3277c5bcac152f6e0e59ce9ae05723a
fallo <fallout.nj@gmail.com>
2019-06-30 18:28:20 +0800

    游戏的初期窗口属性
    2019年6月30日下午

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,12 +2,25 @@ import sys
 
 import pygame
 
+from settings import Settings
+
+from ship import Ship
+
+
 def run_game():
 
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船
+    ship = Ship(screen)
+    # screen = pygame.display.set_mode((1200, 800))
+    # pygame.display.set_caption("Alien Invasion")
 
     # 设置背景色
     bg_color = (230, 230, 230)
@@ -19,7 +32,9 @@ def run_game():
                 sys.exit()
 
         # 每次循环都重绘屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+        # screen.fill(bg_color)
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
